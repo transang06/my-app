968674c099f425d93fcb139045fab9295c98ae99
transang06 <vansang10a6txqt@gmail.com>
2021-06-04 21:10:16 +0700

    ss

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld />
+
+      <Ina/>
+      <Alert/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Ina from "./components/Ina";
+import Alert from "./components/Alert";
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld,
+    Ina,
+    Alert,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
